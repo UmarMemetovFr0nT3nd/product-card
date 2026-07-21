b2cd03ef892f12c68ef3d9a1f5ed21dab5d67640
Umar <umarmemetov99@gmail.com>
2026-07-21 19:33:31 +0300

    add mainjs.js

---
+++ main.js
@@ -0,0 +1,50 @@
+const productCard = document.querySelector('.card');
+const changeColorButton = document.querySelector('#change-card-color-button');
+
+changeColorButton.addEventListener('click', () => {
+  productCard.style.backgroundColor = '#95885d';
+})
+
+const productCards = document.querySelectorAll('.card');
+const changeAllColorButton = document.querySelector('#change-all-cards-color-button');
+
+changeAllColorButton.addEventListener('click', () => {
+  productCards.forEach((card) => card.style.backgroundColor = '#3b3524')
+})
+
+const openGoogleButton = document.querySelector('#go-to-google-button');
+
+openGoogleButton.addEventListener('click', openGoogle);
+
+function openGoogle() {
+  const question = confirm('Вы уверены, что хотите перейти в Google?');
+
+  if (question === true) {
+    window.open('https://www.google.com/');
+  } else {
+    return;
+  }
+}
+
+const outputConsoleButton = document.querySelector('#log-to-console-button');
+
+outputConsoleButton.addEventListener('click', () =>outputConsoleLog('Привет, это сообщение выведено в консоль!'));
+
+function outputConsoleLog(message) {
+  alert('Вы нажали на кнопку "Вывести в консоль". Сообщение будет выведено в консоль.');
+  console.log(message);
+}
+
+const headline = document.querySelector('.headline');
+
+headline.addEventListener('mouseover', function() {
+  console.log(headline);
+})
+
+const thirdCard = document.querySelector('.card:nth-of-type(3)');
+const selectColorButton = document.querySelector('#select-card-color-button');
+
+selectColorButton.addEventListener('click', () => {
+  thirdCard.style.backgroundColor = '';
+  thirdCard.classList.toggle('active');
+});
